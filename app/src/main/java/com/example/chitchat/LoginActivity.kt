22ee440e266b7f283b812a951fba33d2d365ff18
Eduardo <edu21061991@gmail.com>
2021-03-26 20:11:39 +0100

    Añadidos edittext y botones al login

--- app/src/main/java/com/example/chitchat/LoginActivity.kt
+++ app/src/main/java/com/example/chitchat/LoginActivity.kt
@@ -1,13 +1,37 @@
 package com.example.chitchat
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     //Rama eduardo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
+
+        Login_boton.setOnClickListener {
+
+            val email = Correo_edittext.text.toString()
+            val password = Password_edittext.text.toString()
+
+            Log.d("MainActivity","El email es: " +email)
+            Log.d("MainActivity","Password: $password")
+
+        }
+        Registrarse_boton.setOnClickListener{
+            Log.d("MainActivity","Recuperar")
+
+        }
+        RecuperarContraseña_textView.setOnClickListener {
+            Log.d("MainActivity","Recuperar")
+
+        }
+
 
     }
 }
