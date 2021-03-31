287c7f8fb13d28e339c65046d357c1b9f04197da
NoahCaRo <ainhoacaro.007@gmail.com>
2021-03-31 17:11:05 +0200

    Introduccion del boton settings a la ventana principal (junto a su respectiva imagen), ademas de la creacion base del fragment de settings y su viewModel, creacion de string.xml (es)

--- app/src/main/java/com/example/chitchat/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/chitchat/ui/settings/SettingsFragment.kt
@@ -1,4 +1,32 @@
 package com.example.chitchat.ui.settings
 
-class SettingsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.chitchat.R
+import com.example.chitchat.ui.gallery.GalleryViewModel
+
+class SettingsFragment : Fragment(){
+    private lateinit var settingsViewModel: SettingsViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        settingsViewModel =
+                ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val textView: TextView = root.findViewById(R.id.text_gallery)
+        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
 }
