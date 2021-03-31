abc96e38c863e66779a1e6b35ae467e80dafcf8c
Eduardo <edu21061991@gmail.com>
2021-03-31 14:44:52 +0200

    Botones puestos

--- app/src/main/java/com/example/chitchat/login/LoginActivity.kt
+++ app/src/main/java/com/example/chitchat/login/LoginActivity.kt
@@ -7,6 +7,14 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chitchat.MainActivity
 import com.example.chitchat.R
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -15,10 +23,47 @@ import kotlinx.android.synthetic.main.activity_login.*
 class LoginActivity : AppCompatActivity() {
 
     private var auth: FirebaseAuth = Firebase.auth
-
+    private val callbackManager = CallbackManager.Factory.create()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Boton que inicia sesion con Google
+        ///////////////////////////Por aki
+        boton_google.setOnClickListener {
+            val configuraciongoogle = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+
+        }
+
+        //Boton que inicia sesion con Facebook
+        boton_fb.setOnClickListener {
+            LoginManager.getInstance().logInWithReadPermissions(this, listOf("email"))
+            LoginManager.getInstance()
+                .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                    override fun onSuccess(result: LoginResult?) {
+
+                    }
+
+                    override fun onCancel() {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onError(error: FacebookException?) {
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "No se pudo iniciar sesion con Facebook",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                })
+
+
+        }
         Login_boton.setOnClickListener {
             if (Correo_edittext.text.toString().isEmpty() || Password_edittext.text.toString()
                     .isEmpty()
@@ -35,7 +80,11 @@ class LoginActivity : AppCompatActivity() {
                             val intent = Intent(this, MainActivity::class.java)
                             startActivity(intent)
                         } else {
-                            Toast.makeText(this, "Usuario o Password incorrecto", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                this,
+                                "Usuario o Password incorrecto",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
                     }
             }
@@ -51,3 +100,5 @@ class LoginActivity : AppCompatActivity() {
     }
 
 }
+
+
