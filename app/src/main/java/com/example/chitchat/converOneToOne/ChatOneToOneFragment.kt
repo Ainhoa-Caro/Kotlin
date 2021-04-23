7aa6ce2b2446e2da1fc4ac4d4e7af974bf6e885f
mhl1985 <mhl1985@github.com>
2021-04-23 22:44:16 +0200

    Algunas mejoras

--- app/src/main/java/com/example/chitchat/converOneToOne/ChatOneToOneFragment.kt
+++ app/src/main/java/com/example/chitchat/converOneToOne/ChatOneToOneFragment.kt
@@ -1,23 +1,20 @@
 package com.example.chitchat.converOneToOne
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
-import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chitchat.R
 import com.example.chitchat.adapters.MessageAdapter
-import com.example.chitchat.login.LoginFragmentDirections
-import com.example.chitchat.pojos.Conversacion
 import com.example.chitchat.pojos.Mensaje
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.GenericTypeIndicator
+import com.google.firebase.database.Query
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_chat_one_to_one.*
@@ -28,8 +25,7 @@ class ChatOneToOneFragment : Fragment() {
     private lateinit var chatId: String
     private lateinit var userLogueadoId: String
 
-
-    //Variable de enlace con base de datos
+    //Variables de enlace con base de datos
     private val auth: FirebaseAuth = Firebase.auth
     private val database : DatabaseReference = Firebase.database.reference
 
@@ -41,6 +37,7 @@ class ChatOneToOneFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        //Recibimos parámetro
         chatId = requireArguments().getString("idChat").toString()
         //Toast de comprobación paso de parámetro:
         Toast.makeText(context, chatId, Toast.LENGTH_LONG).show()
@@ -50,43 +47,44 @@ class ChatOneToOneFragment : Fragment() {
         //Enlazamos Fragment con su Layout
         return inflater.inflate(R.layout.fragment_chat_one_to_one, container, false)
 
-    //initViews()
+    initViews()
     }
-    /Configuración de RecyclerView con su MensajeAdapter
+
+    //Configuración de RecyclerView con su MensajeAdapter
     private fun initViews(){
+        msjRecylerView.layoutManager = LinearLayoutManager(context)
+        msjRecylerView.adapter = MessageAdapter(userLogueadoId)
 
-
-
-
-        msjRecylerView.layoutManager = LinearLayoutManager(this)
-        msjRecylerView.adapter = MessageAdapter(user)
-
+        //Cuando pulsemos el botón enviar vamos a "enviarMensaje"
         enviarMsjButton.setOnClickListener { sendMessage() }
 
-        val chatRef = db.collection("chats").document(chatId)
 
-        chatRef.collection("mensajes").orderBy("dob", Query.Direction.ASCENDING)
-                .get()
-                .addOnSuccessListener { messages ->
-                    val listMessages = messages.toObjects(Mensaje::class.java)
-                    (msjRecylerView.adapter as MessageAdapter).setData(listMessages)
+
+
+        database.child("chats").child(chatId).child("mensajes").orderByChild("fecha")
+            .get()
+            .addOnSuccessListener { messages ->
+                val listaMensajes: List<Mensaje>? = messages.getValue(object : GenericTypeIndicator<List<Mensaje>>(){})
+                if (listaMensajes != null) {
+                        (msjRecylerView.adapter as MessageAdapter).setData(listaMensajes)
                 }
+            }
 
-        chatRef.collection("messages").orderBy("dob", Query.Direction.ASCENDING)
-                .addSnapshotListener { messages, error ->
-                    if(error == null){
-                        messages?.let {
-                            val listMessages = it.toObjects(Mensaje::class.java)
-                            (msjRecylerView.adapter as MessageAdapter).setData(listMessages)
-                        }
+        database.child("chats").child(chatId).child("mensajes").orderByChild("fecha")
+            .addSnapshotListener { messages, error ->
+                if(error == null){
+                    messages?.let {
+                        val listMessages = it.toObjects(Mensaje::class.java)
+                        (msjRecylerView.adapter as MessageAdapter).setData(listMessages)
                     }
                 }
+            }
     }
 
     private fun sendMessage(){
         val message = Mensaje(
                 message = msjTextField.text.toString(),
-                from = user
+                from = userLogueadoId
 
                 //Meter un comprobador isEmpty???
 
